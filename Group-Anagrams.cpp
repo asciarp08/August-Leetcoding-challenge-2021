0ba7e6c57de4a40359e38a97742ddf1b244afd9a
Anjali R Pahal <39112406+asciarp08@users.noreply.github.com>
2021-08-12 17:18:09 +0530

    Update Group-Anagrams.cpp

--- Group-Anagrams.cpp
+++ Group-Anagrams.cpp
@@ -1,4 +1,59 @@
 /**
+Time = O(n*(k+c))
+Space = O(n*(k+c))
+n is the number of strings in strs
+k is the size of the largest string in strs
+c is for characters in count string- which will have the count of characters stored
+**/
+
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        unordered_map<string,int> groupId; //count string-key, groupId-value
+        vector<vector<string>> groups;
+        int idx = 0;
+        
+        for(auto s:strs){
+            
+            string t = "";
+            vector<int> charCount(26,0);
+            for(auto c:s){
+                charCount[c-'a']++;
+            }
+            for(int i= 0; i<26; i++){
+                t+=to_string(charCount[i]);
+                t+="#";
+            }
+            
+            if(groupId.count(t)){
+                groups[groupId[t]].push_back(s);
+            }
+            else{
+                groupId[t] = idx;
+                vector<string> newGroup;
+                newGroup.push_back(s);
+                groups.push_back(newGroup);
+                idx++;
+            }
+            
+        }
+        
+        return groups;
+        
+    }
+};
+
+
+
+
+
+
+
+
+
+/**
 Time = O(klogk * n)
 Space = O(n*k)
 n is the number of strings in strs
@@ -8,25 +63,24 @@ k is the size of the largest string in strs
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<int>>groups;
+        map<string,int> groupId;
         
-        for(int i = 0; i<strs.size(); i++){
-            string t = strs[i];
+        vector<vector<string>>groups;
+        int idx = 0;
+        for(auto s:strs){
+            string t = s;
             sort(t.begin(), t.end());
-            
-            groups[t].push_back(i);
-        }
-        
-        vector<vector<string>>ans;
-        
-        for(auto [_, pos]:groups){
-            vector<string>tmp;
-            
-            for(auto i:pos){
-                tmp.push_back(strs[i]);
+            if(groupId.count(t)){
+                groups[groupId[t]].push_back(s);
             }
-            ans.push_back(tmp);
+            else{
+                vector<string> newGroup;
+                newGroup.push_back(s);
+                groups.push_back(newGroup);
+                groupId[t] = idx;
+                idx++;
+            }
         }
-        return ans;
+        return groups;
     }
 };
